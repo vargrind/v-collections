ff0b4763bea6551a770db88f88473cfac76f323f
Kevin Zhang <108461102+vargrind@users.noreply.github.com>
2023-06-11 22:32:54 +0000

    focus on circular first

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,16 @@
-pub mod btree;
+// pub mod btree;
 pub mod circular;
-pub mod rbtree;
-pub mod trie;
-pub mod vector;
+// pub mod rbtree;
+// pub mod trie;
+// pub mod vector;
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    // #[test]
+    // fn it_works() {
+    //     let result = add(2, 2);
+    //     assert_eq!(result, 4);
+    // }
 }
