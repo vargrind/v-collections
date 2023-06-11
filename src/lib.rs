9c83deeb2dcf28298eaf5bc9f591734c0bed7dd2
Kevin Zhang <108461102+vargrind@users.noreply.github.com>
2023-06-11 22:57:18 +0000

    begin phase 2

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,10 @@
 // pub mod btree;
 pub mod circular;
 // pub mod rbtree;
-// pub mod trie;
+pub mod trie;
 // pub mod vector;
 
 #[cfg(test)]
 mod tests {
     
-
-    // #[test]
-    // fn it_works() {
-    //     let result = add(2, 2);
-    //     assert_eq!(result, 4);
-    // }
 }
